852fd443d50aa93c6117f7814cd70724436b2c83
Vikram rathod <yadavavikramrathod03@gmail.com>
2026-06-17 10:24:23 +0530

    Add simple calculator functionality

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,24 @@
+print("Simple Calculator")
 
+num1 = float(input("Enter first number: "))
+op = input("Enter operation (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print("Answer =", num1 + num2)
+
+elif op == "-":
+    print("Answer =", num1 - num2)
+
+elif op == "*":
+    print("Answer =", num1 * num2)
+
+elif op == "/":
+    if num2 != 0:
+        print("Answer =", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid operation")
+  
